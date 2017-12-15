0d70ed2dbf51d88545ea529e9c83a1eb43428e5c
nar0909 <narenprathipati@hotmail.com>
2017-12-15 14:45:15 +1100

    Add files via upload

---
+++ Basic-Chat-master/Basic Chat/UUIDKey.swift
@@ -0,0 +1,20 @@
+//
+//  UUIDKey.swift
+//  Basic Chat
+//
+//  Created by Trevor Beaton on 12/3/16.
+//  Copyright © 2016 Vanguard Logic LLC. All rights reserved.
+//
+
+import CoreBluetooth
+//Uart Service uuid
+
+
+let kBLEService_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+let kBLE_Characteristic_uuid_Tx = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+let kBLE_Characteristic_uuid_Rx = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+let MaxCharacters = 20
+
+let BLEService_UUID = CBUUID(string: kBLEService_UUID)
+let BLE_Characteristic_uuid_Tx = CBUUID(string: kBLE_Characteristic_uuid_Tx)//(Property = Write without response)
+let BLE_Characteristic_uuid_Rx = CBUUID(string: kBLE_Characteristic_uuid_Rx)// (Property = Read/Notify)
---
+++ Basic-Chat-master/Basic Chat/UartModuleViewController.swift
@@ -0,0 +1,226 @@
+//
+//  UartModuleViewController.swift
+//  Basic Chat
+//
+//  Created by Trevor Beaton on 12/4/16.
+//  Copyright © 2016 Vanguard Logic LLC. All rights reserved.
+//
+
+
+
+
+
+import UIKit
+import CoreBluetooth
+
+class UartModuleViewController: UIViewController, CBPeripheralManagerDelegate, UITextViewDelegate, UITextFieldDelegate {
+    
+    //UI
+    @IBOutlet weak var baseTextView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var switchUI: UISwitch!
+    //Data
+    var peripheralManager: CBPeripheralManager?
+    var peripheral: CBPeripheral!
+    private var consoleAsciiText:NSAttributedString? = NSAttributedString(string: "")
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Create and start the peripheral manager
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        //-Notification for updating the text view with incoming text
+        updateIncomingData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.baseTextView.text = ""
+        
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // peripheralManager?.stopAdvertising()
+        // self.peripheralManager = nil
+        super.viewDidDisappear(animated)
+        //NotificationCenter.default.removeObserver(self)
+        
+    }
+    
+    func updateIncomingData () {
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "Notify"), object: nil , queue: nil){
+            notification in
+            let appendString = "\n"
+            let myFont = UIFont(name: "Helvetica Neue", size: 15.0)
+            let myAttributes2 = [NSFontAttributeName: myFont!, NSForegroundColorAttributeName: UIColor.red]
+            let attribString = NSAttributedString(string: "[Incoming]: " + (characteristicASCIIValue as String) + appendString, attributes: myAttributes2)
+            let newAsciiText = NSMutableAttributedString(attributedString: self.consoleAsciiText!)
+            self.baseTextView.attributedText = NSAttributedString(string: characteristicASCIIValue as String , attributes: myAttributes2)
+            
+            newAsciiText.append(attribString)
+            
+            self.consoleAsciiText = newAsciiText
+            self.baseTextView.attributedText = self.consoleAsciiText
+            
+        }
+    }
+    
+    @IBAction func clickSendAction(_ sender: AnyObject) {
+        outgoingData()
+        
+    }
+    
+    
+    
+    func outgoingData () {
+        let appendString = "\n"
+        
+        let inputText = inputTextField.text
+        
+        let myFont = UIFont(name: "Helvetica Neue", size: 15.0)
+        let myAttributes1 = [NSFontAttributeName: myFont!, NSForegroundColorAttributeName: UIColor.blue]
+        
+        //Lets send some byte data when continue button is pressed to ble tx side
+        let bytes : [UInt8] = [ 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77 ]
+        let Transmitdata = NSData(bytes: bytes, length: bytes.count)
+        
+        peripheral.writeValue(Transmitdata as Data, for: txCharacteristic!, type: CBCharacteristicWriteType.withoutResponse)
+        print("Data Sent to Transmit Side ",Transmitdata)
+
+
+
+
+        //Now lets check for received byte response packet from the ble rx side
+        let ReceivedData = rxCharacteristic?.value
+        if let ReceivedData = ReceivedData {
+        let ReceivedNoOfBytes = ReceivedData.count
+        var ReceivedByteArray = [UInt8](repeating: 0, count: ReceivedNoOfBytes)
+        (ReceivedData as NSData).getBytes(&ReceivedByteArray, length: ReceivedNoOfBytes)
+        print("Data from Received side ",ReceivedByteArray)
+
+        
+        //Now lets check if we get our desired/expected response from the ble rx side and only then lets continue to next view
+        
+        
+            if(ReceivedByteArray[0] == 0x77 && ReceivedByteArray[1] == 0x66 && ReceivedByteArray[2] == 0x55 && ReceivedByteArray[3] == 0x44 && ReceivedByteArray[4] == 0x33){
+                
+                //CNT is the name of the "Segue" branch -  present modally
+                performSegue(withIdentifier: "CNT", sender: self)
+                
+                
+            }
+        
+        }
+        
+        
+        let attribString = NSAttributedString(string: "[Outgoing]: " + inputText! + appendString, attributes: myAttributes1)
+        let newAsciiText = NSMutableAttributedString(attributedString: self.consoleAsciiText!)
+        newAsciiText.append(attribString)
+        
+        consoleAsciiText = newAsciiText
+        baseTextView.attributedText = consoleAsciiText
+        //erase what's in the text field
+        inputTextField.text = ""
+        
+    }
+    
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.destination is ButtonCNT
+            {
+                
+                let vc1 = segue.destination as? ButtonCNT
+                
+                vc1?.peripheral = blePeripheral
+                
+            }
+            
+        }
+
+        
+        
+        
+    // Write functions
+    func writeValue(data: String){
+        let valueString = (data as NSString).data(using: String.Encoding.utf8.rawValue)
+        //change the "data" to valueString
+        if let blePeripheral = blePeripheral{
+            if let txCharacteristic = txCharacteristic {
+                blePeripheral.writeValue(valueString!, for: txCharacteristic, type: CBCharacteristicWriteType.withResponse)
+            }
+        }
+    }
+    
+    func writeCharacteristic(val: Int8){
+        var val = val
+        let ns = NSData(bytes: &val, length: MemoryLayout<Int8>.size)
+        blePeripheral!.writeValue(ns as Data, for: txCharacteristic!, type: CBCharacteristicWriteType.withResponse)
+    }
+    
+    
+    
+    //MARK: UITextViewDelegate methods
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        if textView === baseTextView {
+            //tapping on consoleview dismisses keyboard
+            inputTextField.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x:0, y:250), animated: true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x:0, y:0), animated: true)
+    }
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        if peripheral.state == .poweredOn {
+            return
+        }
+        print("Peripheral manager is running")
+    }
+    
+    //Check when someone subscribe to our characteristic, start sending the data
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+        print("Device subscribe to characteristic")
+    }
+    
+    //This on/off switch sends a value of 1 and 0 to the Arduino
+    //This can be used as a switch or any thing you'd like
+    @IBAction func switchAction(_ sender: Any) {
+        if switchUI.isOn {
+            print("On ")
+            writeCharacteristic(val: 1)
+        }
+        else
+        {
+            print("Off")
+            writeCharacteristic(val: 0)
+            print(writeCharacteristic)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        outgoingData()
+        return(true)
+    }
+    
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        if let error = error {
+            print("\(error)")
+            return
+        }
+    }
+}
+
